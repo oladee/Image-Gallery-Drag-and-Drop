110686fff479ff9964b73b991da73d200875178a
oladee <deemajor230600@gmail.com>
2023-09-20 12:57:19 +0100

    included redirects file

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,57 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 import Homepage from "./components/Homepage";
-import { DndProvider } from "react-dnd";
+import {
+  DndProvider,
+  TouchTransition,
+  MouseTransition,
+} from "react-dnd-multi-backend";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import update from "immutability-helper";
-import {TouchBackend }from "react-dnd-touch-backend";
-import { createClient } from 'pexels'
+import { TouchBackend } from "react-dnd-touch-backend";
+import { createClient } from "pexels";
 
 function App() {
-  const isTouchDevice = () => {
-    if ("ontouchstart" in window) {
-      return true;
-    }
-    return false;
+
+ const HTML5toTouch = {
+    backends: [
+      {
+        id: "html5",
+        backend: HTML5Backend,
+        transition: MouseTransition,
+      },
+      {
+        id: "touch",
+        backend: TouchBackend,
+        options: { enableMouseEvents: true },
+        preview: true,
+        transition: TouchTransition,
+      },
+    ],
   };
 
-  const backendForDND = isTouchDevice() ? TouchBackend : HTML5Backend;
+  const [imageData, setImageData] = useState([]);
 
-  const [imageData, setImageData] = useState([])
+  useEffect(() => {
+    const client = createClient(
+      "QPBd3qLXlS8mfqMwhwVqmMuMuZbau4tEMku2VabcadkOBKJTF2VnEJfI"
+    );
 
-  useEffect(()=>{
-    const client = createClient('QPBd3qLXlS8mfqMwhwVqmMuMuZbau4tEMku2VabcadkOBKJTF2VnEJfI');
-
-        client.photos.curated({ per_page: 8 }).then(photos => setImageData(photos.photos));
-  },[])
+    client.photos
+      .curated({ per_page: 8 })
+      .then((photos) => setImageData(photos.photos));
+  }, []);
 
   const moveImage = (dragIndex, hoverIndex) => {
     const draggedImage = imageData[dragIndex];
 
     setImageData(
       update(imageData, {
-        $splice: [[dragIndex, 1], [hoverIndex, 0, draggedImage]]
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, draggedImage],
+        ],
       })
     );
   };
@@ -43,7 +63,7 @@ function App() {
         <Route
           path="/home"
           element={
-            <DndProvider backend={backendForDND}>
+            <DndProvider options={HTML5toTouch}>
               <Homepage moveImage={moveImage} imageData={imageData} />
             </DndProvider>
           }
